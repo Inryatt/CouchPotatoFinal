91cb56702c08cc596eede3c2a64d8e13e4404a56
Inryatt <cffonseca@ua.pt>
2020-05-31 18:53:43 +0100

    fixed json

--- js/index.js
+++ js/index.js
@@ -3,17 +3,82 @@ var dieta =false;
 var vegetariano = true;
 let alimentos =[];
 
-$.getJSON( "recipes.json", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push(  key + " : " + val  );
-  });
-  console.log(items);
- 
- 
-});
-
-
+var receitas={
+  receitas: {
+    paocaseiro: {
+      name: 'Pão Caseiro',
+      autor: 'João Mateus',
+      time: '1 hora',
+      dificuldade: 1,
+      Ingredientes: {
+        farinhaTrigo: {
+          tipo: 'farinha',
+          propriedades: [
+            'trigo',
+            'sem fermento'
+          ],
+          quantidade: 1000,
+          texto: '1 kg de farinha de trigo sem fermento'
+        },
+        leite: {
+          tipo: 'leite',
+          propriedades: [
+            'morno'
+          ],
+          quantidade: 400,
+          texto: '400ml de leite morno'
+        },
+        ovo: {
+          tipo: 'farinha',
+          propriedades: [],
+          quantidade: 2,
+          texto: '2 ovos (remova a película ou pele da gema'
+        },
+        fermento: {
+          tipo: 'farinha',
+          propriedades: [
+            'biológico',
+            'fresco'
+          ],
+          quantidade: 45,
+          texto: '45g de fermento biológico fresco'
+        },
+        'açúcar': {
+          tipo: 'tempero',
+          propriedades: [
+            'acucar'
+          ],
+          quantidade: 200,
+          texto: '1 chávena de açúcar'
+        },
+        sal: {
+          tipo: 'tempero',
+          propriedades: [
+            'sal'
+          ],
+          quantidade: 10,
+          texto: '1/2 colher (sopa) de sal'
+        }
+      },
+      'Preparação': {
+        '1': '1. Bata todos os ingredientes juntos menos o sal e a farinha.',
+        '2': '2. Num recipiente largo, meta a sua mistura e adicione metade da farinha, mexendo com uma colher de pau.',
+        '3': '3. Guarde uma chávena de farinha e adicione a restante e o sal à mistura.',
+        '4': '4. Neste momento ligue seu forno em fogo baixo (e desligue-o após 5 minutos).',
+        '5': '5. Enfarinhe uma superfície e sove a massa com carinho até que ela desgrude das mãos.',
+        '6': '6. Divida a massa em duas partes iguais e abra cada uma com um rolo, enfarinhando a superfície novamente.',
+        '7': '7. Leve os pães ao forno que você aqueceu por 5 minutos e desligou.',
+        '8': '8. Deixe-os descansar, para que cresçam, estimulados pelo calor morno durante 20 a 30 minutos.',
+        '9': '9. Mesmo que a massa não tenha crescido após esse tempo, ligue o forno em temperatura média (180º C) e deixe ela assar por uns 30, 40 ou 45 minutos (depende do seu forno).',
+        '10': '10. Deixe esfriar dentro ou fora do forno.'
+      },
+      isVeg: true,
+      kCal: 500
+    }
+  }
+}
+//this is huge
+//console.log(receitas); test stuff
 $('#simveg').on('click change', function() {
  // alert("veg true");
   vegetariano = true;
