3c81b58ab7dfbb9862a1d92597c9b36a91f7d9a4
Inryatt <cffonseca@ua.pt>
2020-05-31 11:48:52 +0100

    started work on recipe modals + javascript fuckery

--- js/index.js
+++ js/index.js
@@ -7,3 +7,17 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   } 
+
+  //recipe modal call
+
+  $('#theModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('name') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('#recipeName').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+  })
+
+  
