5cd3900eef2e9a374c734cf3097ec94fad17eabe
Andreia <andreia.portela.2001@gmail.com>
2020-06-01 20:00:08 +0100

    Vou comer

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ var receitas={
       name: 'Pão Caseiro',
       autor: 'João Mateus',
       time: '1 hora',
+      portions: '6 pães',
       dificuldade: 1,
       Ingredientes: {
         farinhaTrigo: {
@@ -32,7 +33,7 @@ var receitas={
           tipo: 'farinha',
           propriedades: [],
           quantidade: 2,
-          texto: '2 ovos (remova a película ou pele da gema'
+          texto: '2 ovos (remova a película ou pele da gema)'
         },
         fermento: {
           tipo: 'farinha',
@@ -75,6 +76,78 @@ var receitas={
       isVeg: true,
       kCal: 500
     
+  },
+  carbonara:{
+    name: 'Massa carbonara',
+    autor: 'Pingo Doce',
+    time:'20 minutos',
+    portions: '3 pessoas',
+    dificuldade: 1,
+    Ingredientes: {
+      farinhaTrigo: {
+        tipo: 'farinha',
+        propriedades: [
+          'trigo',
+          'sem fermento'
+        ],
+        quantidade: 1000,
+        texto: '1 kg de farinha de trigo sem fermento'
+      },
+      leite: {
+        tipo: 'leite',
+        propriedades: [
+          'morno'
+        ],
+        quantidade: 400,
+        texto: '400ml de leite morno'
+      },
+      ovo: {
+        tipo: 'farinha',
+        propriedades: [],
+        quantidade: 2,
+        texto: '2 ovos (remova a película ou pele da gema)'
+      },
+      fermento: {
+        tipo: 'farinha',
+        propriedades: [
+          'biológico',
+          'fresco'
+        ],
+        quantidade: 45,
+        texto: '45g de fermento biológico fresco'
+      },
+      açúcar: {
+        tipo: 'tempero',
+        propriedades: [
+          'acucar'
+        ],
+        quantidade: 200,
+        texto: '1 chávena de açúcar'
+      },
+      sal: {
+        tipo: 'tempero',
+        propriedades: [
+          'sal'
+        ],
+        quantidade: 10,
+        texto: '1/2 colher (sopa) de sal'
+      }
+    },
+    Preparação: {
+      '1': '1. Bata todos os ingredientes juntos menos o sal e a farinha.',
+      '2': '2. Num recipiente largo, meta a sua mistura e adicione metade da farinha, mexendo com uma colher de pau.',
+      '3': '3. Guarde uma chávena de farinha e adicione a restante e o sal à mistura.',
+      '4': '4. Neste momento ligue seu forno em fogo baixo (e desligue-o após 5 minutos).',
+      '5': '5. Enfarinhe uma superfície e sove a massa com carinho até que ela desgrude das mãos.',
+      '6': '6. Divida a massa em duas partes iguais e abra cada uma com um rolo, enfarinhando a superfície novamente.',
+      '7': '7. Leve os pães ao forno que você aqueceu por 5 minutos e desligou.',
+      '8': '8. Deixe-os descansar, para que cresçam, estimulados pelo calor morno durante 20 a 30 minutos.',
+      '9': '9. Mesmo que a massa não tenha crescido após esse tempo, ligue o forno em temperatura média (180º C) e deixe ela assar por uns 30, 40 ou 45 minutos (depende do seu forno).',
+      '10': '10. Deixe esfriar dentro ou fora do forno.'
+    },
+    isVeg: true,
+    kCal: 500
+
   },
   outrareceita:{}
 }
@@ -147,9 +220,12 @@ $('#theModal').on('show.bs.modal', function (event) {
      // alert('beep');
        details = receitas[property] ;
       console.log(details);
+      var nome = details['name'];
     var autor = details['autor'];
     console.log(autor);
     var diff = details['dificuldade'];
+    var port = details['portions'];
+    var time = details['time'];
     var ingredientes = details['Ingredientes'];
     for(var item in ingredientes){
       console.log(ingredientes[item]['texto']);
@@ -162,8 +238,16 @@ $('#theModal').on('show.bs.modal', function (event) {
     }
    
   }
-  modal.find('#recipeName').text(name);
-  modal.find('#recipeName').text(name);
+  
+  modal.find('#recipeName').text(nome);
+  modal.find('#recipeAutor').text(autor);
+  modal.find('#recipeDif').text(diff);
+  modal.find('#recipeTime').text(time);
+  modal.find('#recipePor').text(port);
+
+
+ 
+
 
 })
 
