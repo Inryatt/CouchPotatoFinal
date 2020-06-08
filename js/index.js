997660a0281c141d472eebe89ec8081078885e7f
Inryatt <cffonseca@ua.pt>
2020-06-08 10:58:26 +0100

    little fix on compras

--- js/index.js
+++ js/index.js
@@ -2860,21 +2860,25 @@ $('#theModal').on('show.bs.modal', function (event) {
                 if (item in tmpInv) {
                     console.log('necessario:' + ingredientes[item]['quantidade']);
                     console.log('existe' + tmpInv[item]['quantidade']);
-                    console.log((ingredientes[item]['quantidade'] > tmpInv[item]['quantidade']));
+                   // console.log((ingredientes[item]['quantidade'] > tmpInv[item]['quantidade']));
                     if (ingredientes[item]['quantidade'] > tmpInv[item]['quantidade']) {
-                        console.log(ingredientes[item]['quantidade'] + 'MIMIMIMI');
+                       // console.log(ingredientes[item]['quantidade'] + 'MIMIMIMI');
                         $("#ingredientes").append("<li id=\"addCart\" style=\"color:red\">" + ingredientes[item]['texto'] + "<button style=\"border-radius: 50px ; background:transparent;\" id=\"addCart\">&#x1f6d2</button></li>");
                         //A minha ideia aqui em baixo é que quando clicas no carrinho, o item é adicionado a uma lista
                         //no local storage e quando o carrinho de compras abrisse, iamos buscar os itens a esta lista
                         // e já os tinhamos no carrinho
                         //however, não tou a conseguir mandar o item ou até o nome do item para uma lista
-                        var nomee=ingredientes[item];
-                        $('#addCart').click(function(nomee){
+
+
+                        //Camila: on it
+                        var nomee=item;
+                        console.log("nomee="+item);
+                        $('#addCart').click(function(){
             
                             alert("Adicionado ao carrinho com sucesso");
                             compras.push(nomee) ; 
                             
-                            localStorage.setItem("compras", JSON.stringify(compras));
+                            localStorage.compras= compras;
                         });
 
                         //  alert("beepbeep");
@@ -2985,7 +2989,7 @@ $(document).ready(function () {
     var tmpInv = JSON.parse(localStorage.inventario);
 
     for (var item in tmpInv) {
-        console.log(item);
+        //console.log(item);
         $("#ingList").append("<li>" + tmpInv[item]['nome'] + "  [" + tmpInv[item]['quantidade'] + "]</li>");
     }
 })
@@ -3108,92 +3112,8 @@ $("#cycleButton").click(function () {
 
 
 
-function openModal(modalName)
-
-    var tmpInv = JSON.parse(localStorage.inventario);
-
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var name = button.data('name') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    var nome;
-    var autor;
-    var diff;
-    var port;
-    var time;
-    var ingredientes;
-    var passos;
-    var details;
-    $("#ingredientes").empty();
-    $("#receitaEtapas").empty();
 
 
-    for (var property in receitas) {
-        console.log(property);
-        if (property == name) {
-            // alert('beep');
-            details = receitas[property];
-            console.log(details);
-            nome = details['name'];
-            autor = details['autor'];
-            console.log(autor);
-            diff = details['dificuldade'];
-            port = details['portions'];
-            time = details['time'];
-
-            ingredientes = details['Ingredientes'];
-
-
-
-
-
-
-
-
-            for (var item in ingredientes) {
-                //  console.log(JSON.parse(localStorage.inventario));
-                // console.log(ingredientes[item]['texto']);
-                if (item in tmpInv) {
-                    console.log('necessario:' + ingredientes[item]['quantidade']);
-                    console.log('existe' + tmpInv[item]['quantidade']);
-                    console.log((ingredientes[item]['quantidade'] > tmpInv[item]['quantidade']));
-                    if (ingredientes[item]['quantidade'] > tmpInv[item]['quantidade']) {
-                        console.log(ingredientes[item]['quantidade'] + 'MIMIMIMI');
-                        $("#ingredientes").append("<li style=\"color:red\">" + ingredientes[item]['texto'] + "</li>");
-                        //  alert("beepbeep");
-                        // console.log("LAAAAAAALALAALLALALALALAL");
-                    } else {
-                        $("#ingredientes").append("<li>" + ingredientes[item]['texto'] + "</li>");
-
-                    }
-                    //console.log("aaaa");
-                } else {
-                    $("#ingredientes").append("<li style=\"color:red\">" + ingredientes[item]['texto'] + "</li>");
-                }
-                passos = details['Preparação'];
-            }
-            for (var item in passos) {
-                $("#receitaEtapas").append("<li>" + passos[item] + "</li>");
-            }
-            //console.log(autor+" , "+diff +" ," + JSON.stringify(ingredientes));
-
-
-
-        }
-    }
-
-    modal.find('#recipeName').text(nome);
-    modal.find('#recipeAutor').text(autor);
-    modal.find('#recipeDif').text(diff);
-    modal.find('#recipeTime').text(time);
-    modal.find('#recipePor').text(port);
-
-
-
-
-
-});
 $("#cycleButton2").click(function () {
     cycleRecipes2();
 });
